c5f0cb1e95c59fa57fc7889762adaca52b82c927
Patrick <patrick.cerny@gmail.com>
2016-03-06 06:29:51 +0100

    test update

--- link2phone/src/test/java/link2phone/DemoTest.java
+++ link2phone/src/test/java/link2phone/DemoTest.java
@@ -1,11 +1,18 @@
 package link2phone;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class DemoTest {
 
+	@Test
+	public void shouldReturnFalse() {
+		Demo demo = new Demo();
+		assertEquals(false, demo.getBool());
+	}
+	
 	@Test
 	public void shouldReturnTrue()
 	{
